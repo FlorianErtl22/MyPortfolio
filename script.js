f2d64ebc496c50e4a908896280bb41a2977d4289
FlorianErtl22 <mail@florian-ertl.com>
2021-01-30 18:28:56 +0100

    add media

---
+++ script.js
@@ -0,0 +1,10 @@
+// let html = document.getElementById("html");
+// let htmlp = document.getElementById("htmlp");
+// function showInfo(picId){
+//     picId.style.display = "block";
+// }
+// function reset(picId){
+//     picId.style.display = "none";
+// }
+// html.addEventListener('mouseover',showInfo(htmlp));
+// html.addEventListener('mouseout',reset(htmlp));
